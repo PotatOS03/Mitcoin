70a3ce1106b3a129aedcf3456aa1af5a4a88faee
PotatOS03 <37935334+PotatOS03@users.noreply.github.com>
2018-05-23 20:50:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Bot setup
 const botconfig = require("./botconfig.json");
-const tokenfile = require("./token.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
@@ -232,4 +231,4 @@ bot.on("message", async message => {
 });
 
 // Log in to the Discord bot
-bot.login(tokenfile.token);
+bot.login(process.env.BOT_TOKEN);

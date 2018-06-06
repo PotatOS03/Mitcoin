75449309d65d08f5c2afeac1b9bb4ece4bfa53a0
PotatOS03 <37935334+PotatOS03@users.noreply.github.com>
2018-06-06 14:07:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -605,7 +605,7 @@ bot.on("message", async message => {
     client.query("DELETE FROM history");
     client.query(`UPDATE value SET value = ${mitcoinInfo.value}`);
     for (let i in mitcoinInfo.balances) {
-      client.query(`INSERT INTO balances VALUES(${i}, ${mitcoinInfo.balances[i].balance}, ${mitcoinInfo[i].money})`);
+      client.query(`INSERT INTO balances VALUES(${i}, ${mitcoinInfo.balances[i].balance}, ${mitcoinInfo.balances[i].money})`);
     }
     for (let i in mitcoinInfo.blacklist) {
       client.query(`INSERT INTO blacklist VALUES(${mitcoinInfo.blacklist[i]})`);

73d9f471af970bf745dfd0a56e5447d8577f142d
PotatOS03 <37935334+PotatOS03@users.noreply.github.com>
2019-01-01 09:53:58 -0500

    Update index.js
    Quick test x2

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ let mitcoinInfo = {
 
 // Load Mitcoin information from the database
 client.query("SELECT * FROM value", (err, res) => {
-  console.log(res.rows);
+  res.rows[0].demand = 0;
   mitcoinInfo.value = (res.rows[0].value);
 })
 client.query("SELECT * FROM blacklist", (err, res) => {
